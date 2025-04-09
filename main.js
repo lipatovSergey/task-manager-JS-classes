3ee48ebfc4f2fe2326ca1a719de5c165deffb3e2
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-04-09 18:37:15 +0300

    all basic functionality done

--- main.js
+++ main.js
@@ -41,7 +41,12 @@ class TaskList {
 		const data = localStorage.getItem("tasks");
 		if (data) {
 			const parsed = JSON.parse(data);
-			this.tasks = parsed.map(t => new Task(t.title));
+			this.tasks = parsed.map(t => {
+				const task = new Task(t.title);
+				// if task status was completed, set it completed
+				if (t.completed) task.completed = true;
+				return task;
+			});
 		}
 	}
 }
@@ -51,13 +56,36 @@ const taskInput = document.querySelector("#task-input");
 const taskList = document.querySelector("#task-list");
 
 const list1 = new TaskList();
+list1.loadFromLocalStorage();
+renderTasks();
 
 function renderTasks() {
 	taskList.innerHTML = ""; // clean ul
 	list1.tasks.forEach(task => {
 		const li = document.createElement("li");
 		li.textContent = task.title;
+		// delete btn
+		const deleteBtn = document.createElement("button");
+		deleteBtn.textContent = "delete";
+		deleteBtn.addEventListener("click", function (e) {
+			e.preventDefault();
+			list1.deleteTask(task.id);
+			renderTasks();
+		});
+		li.appendChild(deleteBtn);
+		// toggle task status
+		const toggleStatusBtn = document.createElement("button");
+		toggleStatusBtn.textContent = "done";
+		if (task.completed) toggleStatusBtn.classList.add("done");
+		toggleStatusBtn.addEventListener("click", function (e) {
+			e.preventDefault();
+			list1.toggleTask(task.id);
+			this.classList.toggle("done");
+			renderTasks();
+		});
+		li.appendChild(toggleStatusBtn);
 		taskList.appendChild(li);
+		list1.saveToLocalStorage();
 	});
 }
 
@@ -72,30 +100,3 @@ form.addEventListener("submit", function (event) {
 	form.reset();
 	renderTasks();
 });
-
-// ===========================
-// Шаг 3: Работа с DOM
-// ===========================
-
-// Добавить обработчик события submit:
-// - предотвратить перезагрузку
-// - взять значение input
-// - вызвать addTask()
-// - обновить отображение списка
-
-// Добавить функцию renderTasks():
-// - очистить список
-// - пройтись по всем задачам и создать li с кнопками
-// - добавить класс completed, если задача завершена
-// - добавить кнопки для удаления и переключения completed
-
-// Повесить события на кнопки удаления и переключения
-
-// ===========================
-// Шаг 4: Инициализация
-// ===========================
-// Создать экземпляр TaskList
-// Загрузить задачи из localStorage
-// Отрендерить их
-
-// Готово!
